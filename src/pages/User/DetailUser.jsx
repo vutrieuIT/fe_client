7f8352c42f6a72a74080fd122163db1cbc2c9848
vutrieuIT <63440096+vutrieuIT@users.noreply.github.com>
2024-10-30 21:07:55 +0700

    update page detail user (#3)
    * add detail user
    
    * remove DetailUser.css

--- src/pages/User/DetailUser.jsx
+++ src/pages/User/DetailUser.jsx
@@ -1,11 +1,58 @@
+import "./DetailUser.css";
 
 const DetailUser = () => {
-    
-    return (
-        <section>
-            <h1>Thong tin khach hang</h1>
-        </section>
-    );
+  const userInfo = JSON.parse(sessionStorage.getItem("userInfo"))[0];
+
+  return (
+    <div className="d-flex flex-wrap justify-content-center">
+      <h1 className="d-flex w-100 justify-content-center">Thông tin cá nhân</h1>
+
+        <div className="d-flex justify-content-center  w-100" >
+          <div className="w-100" style={{
+            maxWidth: "500px",
+            padding: "20px",
+          }}>
+            <div className="form-group">
+              <label>Họ và tên</label>
+              <input
+                type="text"
+                className="form-control"
+                value={userInfo.name}
+                readOnly
+              />
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="text"
+                className="form-control"
+                value={userInfo.email}
+                readOnly
+              />
+            </div>
+            <div className="form-group">
+              <label>Số điện thoại</label>
+              <input
+                type="text"
+                className="form-control"
+                value={userInfo.phone}
+                readOnly
+              />
+            </div>
+            <div className="form-group">
+              <label>Địa chỉ</label>
+              <input
+                type="text"
+                className="form-control"
+                value={userInfo.address}
+                readOnly
+              />
+            </div>
+          </div>
+        </div>
+
+    </div>
+  );
 };
 
 export default DetailUser;
