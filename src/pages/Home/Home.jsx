b445600ed9860874aa83ad57873a353fd61ffc1e
TrieuVD <vutrieu2002@gmail.com>
2024-12-01 22:34:28 +0700

    update banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -65,50 +65,35 @@ function Home() {
     if (!products.length) {
       getProducts();
     }
-    // if (!newProducts.length) {
-    //   getNewProducts();
-    // }
-    // if (!bestsellingProducts.length) {
-    //   getBestsellingProducts();
-    // }
-    // if (!hotProducts.length) {
-    //   getHotProducts();
-    // }
-  }, [products]);
+    if (!newProducts.length) {
+      getNewProducts();
+    }
+    if (!bestsellingProducts.length) {
+      getBestsellingProducts();
+    }
+    if (!hotProducts.length) {
+      getHotProducts();
+    }
+    // eslint-disable-next-line
+  }, []);
 
   const dispatch = useDispatch();
   // const globalstate = useSelector(state=>state.cartState);
   const { add } = cartSlice.actions;
   const product_newProducts = newProducts.map((product) => {
-    if (product.variations.length === 0) return;
+     if (product.specifications.length === 0) return;
     return (
       <div key={product.id} className="col-xl-3 col-lg-4 col-sm-6">
         <div
           className={`product text-start bg-light mb-3 ${styles.borderProduct} ${styles.paddingImageProduct}`}
         >
           <div className="position-relative mb-3">
-            {product.status == "Sale" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "Mới" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "Hết hàng" && (
-              <div className="badge text-white bg-secondary">
-                {product.status}
-              </div>
-            )}
-            {product.status == "Bán chạy" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "" && (
-              <div className="badge">{product.status}</div>
-            )}
+            <div className="badge">{product.status} logic</div>
             <Link className="d-block" to={`./cua-hang/${product.id}`}>
               <img
                 className={`img-fluid ${styles.borderImageProduct}`}
-                src={`${product.variations[0].image_url}`}
-                alt={product.img}
+                src={`${HOST.concat(product.variants[0].images[0])}`}
+                alt={product.name}
               />
             </Link>
             <div className="product-overlay">
@@ -120,7 +105,7 @@ function Home() {
                 </li>
                 {!hasLogin ? (
                   <li className="list-inline-item m-0 p-0">
-                    <a className="btn btn-sm btn-dark" href={"dang-nhap"}>
+                    <a className="btn btn-sm btn-dark" href={"dang-nhap.html"}>
                       <i className="fa fa-cart-plus"></i> Thêm vào giỏ
                     </a>
                   </li>
@@ -167,42 +152,26 @@ function Home() {
             </Link>
           </h6>
           <p className="text-center mb-1 small text-black">
-            ${product.variations[0].price}
+            {product.specifications[0].price.toLocaleString()} VNĐ
           </p>
         </div>
       </div>
     );
   });
   const product_bestsellingProducts = bestsellingProducts.map((product) => {
-    if (product.variations.length === 0) return;
+     if (product.specifications.length === 0) return;
     return (
       <div key={product.id} className="col-xl-3 col-lg-4 col-sm-6">
         <div
           className={`product text-start bg-light mb-3 ${styles.borderProduct} ${styles.paddingImageProduct}`}
         >
           <div className="position-relative mb-3">
-            {product.status == "Sale" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "Mới" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "Hết hàng" && (
-              <div className="badge text-white bg-secondary">
-                {product.status}
-              </div>
-            )}
-            {product.status == "Bán chạy" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "" && (
-              <div className="badge">{product.status}</div>
-            )}
+            <div className="badge">{product.status} logic</div>
             <Link className="d-block" to={`./cua-hang/${product.id}`}>
               <img
                 className={`img-fluid ${styles.borderImageProduct}`}
-                src={`${product.variations[0].image_url}`}
-                alt={product.img}
+                src={`${HOST.concat(product.variants[0].images[0])}`}
+                alt={product.name}
               />
             </Link>
             <div className="product-overlay">
@@ -214,7 +183,7 @@ function Home() {
                 </li>
                 {!hasLogin ? (
                   <li className="list-inline-item m-0 p-0">
-                    <a className="btn btn-sm btn-dark" href={"dang-nhap"}>
+                    <a className="btn btn-sm btn-dark" href={"dang-nhap.html"}>
                       <i className="fa fa-cart-plus"></i> Thêm vào giỏ
                     </a>
                   </li>
@@ -261,42 +230,26 @@ function Home() {
             </Link>
           </h6>
           <p className="text-center mb-1 small text-black">
-            ${product.variations[0].price}
+            {product.specifications[0].price.toLocaleString()} VNĐ
           </p>
         </div>
       </div>
     );
   });
   const product_hotProducts = hotProducts.map((product) => {
-    if (product.variations.length === 0) return;
+     if (product.specifications.length === 0) return;
     return (
       <div key={product.id} className="col-xl-3 col-lg-4 col-sm-6">
         <div
           className={`product text-start bg-light mb-3 ${styles.borderProduct} ${styles.paddingImageProduct}`}
         >
           <div className="position-relative mb-3">
-            {product.status == "Sale" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "Mới" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "Hết hàng" && (
-              <div className="badge text-white bg-secondary">
-                {product.status}
-              </div>
-            )}
-            {product.status == "Bán chạy" && (
-              <div className="badge text-white bg-danger">{product.status}</div>
-            )}
-            {product.status == "" && (
-              <div className="badge">{product.status}</div>
-            )}
+            <div className="badge">{product.status} logic</div>
             <Link className="d-block" to={`./cua-hang/${product.id}`}>
               <img
                 className={`img-fluid ${styles.borderImageProduct}`}
-                src={`${product.variations[0].image_url}`}
-                alt={product.img}
+                src={`${HOST.concat(product.variants[0].images[0])}`}
+                alt={product.name}
               />
             </Link>
             <div className="product-overlay">
@@ -308,7 +261,7 @@ function Home() {
                 </li>
                 {!hasLogin ? (
                   <li className="list-inline-item m-0 p-0">
-                    <a className="btn btn-sm btn-dark" href={"dang-nhap"}>
+                    <a className="btn btn-sm btn-dark" href={"dang-nhap.html"}>
                       <i className="fa fa-cart-plus"></i> Thêm vào giỏ
                     </a>
                   </li>
@@ -355,7 +308,7 @@ function Home() {
             </Link>
           </h6>
           <p className="text-center mb-1 small text-black">
-            ${product.variations[0].price}
+            {product.specifications[0].price.toLocaleString()} VNĐ
           </p>
         </div>
       </div>
@@ -433,7 +386,7 @@ function Home() {
             </Link>
           </h6>
           <p className="text-center mb-1 small text-black">
-            ${product.specifications[0].price}
+            {product.specifications[0].price.toLocaleString()} VNĐ
           </p>
         </div>
       </div>
@@ -441,129 +394,11 @@ function Home() {
   });
   return (
     <section>
-      {/*<!--  Modal -->*/}
-      <div className="modal fade" id="productView" tabIndex="-1">
-        <div className="modal-dialog modal-lg modal-dialog-centered">
-          <div className="modal-content overflow-hidden border-0">
-            <button
-              className="btn-close p-4 position-absolute top-0 end-0 z-index-20 shadow-0"
-              type="button"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-            <div className="modal-body p-0">
-              <div className="row align-items-stretch">
-                <div className="col-lg-6 p-lg-0">
-                  <a
-                    className={`glightbox product-view d-block h-100 bg-cover bg-center ${styles.linkStyle}`}
-                    href="img/product-5.jpg"
-                    data-gallery="gallery1"
-                    data-glightbox="Red digital smartwatch"
-                  ></a>
-                  <a
-                    className="glightbox d-none"
-                    href="img/product-5-alt-1.jpg"
-                    data-gallery="gallery1"
-                    data-glightbox="Red digital smartwatch"
-                  ></a>
-                  <a
-                    className="glightbox d-none"
-                    href="img/product-5-alt-2.jpg"
-                    data-gallery="gallery1"
-                    data-glightbox="Red digital smartwatch"
-                  ></a>
-                </div>
-                <div className="col-lg-6">
-                  <div className="p-4 my-md-4">
-                    <ul className="list-inline mb-2">
-                      <li className="list-inline-item m-0">
-                        <i className="fas fa-star small text-warning"></i>
-                      </li>
-                      <li className="list-inline-item m-0 1">
-                        <i className="fas fa-star small text-warning"></i>
-                      </li>
-                      <li className="list-inline-item m-0 2">
-                        <i className="fas fa-star small text-warning"></i>
-                      </li>
-                      <li className="list-inline-item m-0 3">
-                        <i className="fas fa-star small text-warning"></i>
-                      </li>
-                      <li className="list-inline-item m-0 4">
-                        <i className="fas fa-star small text-warning"></i>
-                      </li>
-                    </ul>
-                    <h2 className="h4">Red digital smartwatch</h2>
-                    <p className="text-muted">$250</p>
-                    <p className="text-sm mb-4">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      In ut ullamcorper leo, eget euismod orci. Cum sociis
-                      natoque penatibus et magnis dis parturient montes nascetur
-                      ridiculus mus. Vestibulum ultricies aliquam convallis.
-                    </p>
-                    <div className="row align-items-stretch mb-4 gx-0">
-                      <div className="col-sm-7">
-                        <div className="border d-flex align-items-center justify-content-between py-1 px-3">
-                          <span className="small text-uppercase text-gray mr-4 no-select">
-                            Quantity
-                          </span>
-                          <div className="quantity">
-                            <button className="dec-btn p-0">
-                              <i className="fas fa-caret-left"></i>
-                            </button>
-                            <input
-                              className="form-control border-0 shadow-0 p-0"
-                              type="text"
-                              onChange={() => {}}
-                              value="1"
-                            />
-                            <button className="inc-btn p-0">
-                              <i className="fas fa-caret-right"></i>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-sm-5">
-                        <a
-                          className="btn btn-dark btn-sm w-100 h-100 d-flex align-items-center justify-content-center px-0"
-                          href="cart.html"
-                        >
-                          Add to cart
-                        </a>
-                      </div>
-                    </div>
-                    <a
-                      className="btn btn-link text-dark text-decoration-none p-0"
-                      href="#!"
-                    >
-                      <i className="far fa-heart me-2"></i>Add to wish list
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       {/* <!-- HERO SECTION-->*/}
       <div className="container">
         <section
           className={`hero pb-3 bg-cover bg-center d-flex align-items-center ${styles.bannerStyle}`}
         >
-          <div className="container py-5">
-            <div className="row px-4 px-lg-5">
-              <div className="col-lg-6">
-                <p className="text-muted small text-uppercase mb-2">
-                  New Inspiration 2020
-                </p>
-                <h1 className="h2 text-uppercase mb-3">
-                  20% off on new season
-                </h1>
-                <a className="btn btn-dark rounded-3" href="shop.html">
-                  Xem ngay những sản phẩm mới
-                </a>
-              </div>
-            </div>
-          </div>
         </section>
         {/*<!-- NEW PRODUCTS-->*/}
         <section className="py-5">
